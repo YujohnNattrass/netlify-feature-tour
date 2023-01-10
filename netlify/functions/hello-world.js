fb9398ea44876b4b0bf210354d60479aa21f57b2
YujohnNattrass <46704329+YujohnNattrass@users.noreply.github.com>
2023-01-10 10:49:02 -0500

    Update hello-world.js

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,6 +2,6 @@ exports.handler = async () => {
   const mySecret = process.env.MY_SECRET;
   return {
     statusCode: 200,
-    body: `hello world!!!!123`,
+    body: `hello world!!!!123abcd`,
   };
 };
